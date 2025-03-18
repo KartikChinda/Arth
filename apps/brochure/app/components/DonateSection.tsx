53559f56ce84a96c9db1a0bebdbe4524ab97df82
Kartik Chinda <kartik.chinda@gmail.com>
2025-03-18 14:40:01 +0530

    Feat: Added modal popup to Donate and UI changes.

--- apps/brochure/app/components/DonateSection.tsx
+++ apps/brochure/app/components/DonateSection.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
+import { useClickOutside } from "../hooks/useClickOutside";
+import ComingSoonModal from "./ComingSoonModal";
 
 const DonateSection = () => {
+  const [openModal, setopenModal] = useState(false);
+  const modalRef = useRef(null);
+  useClickOutside(modalRef, () => setopenModal(false));
   return (
     <section id="donate" className="flex justify-center items-center h-screen ">
+      {openModal && <ComingSoonModal setModalOpen={setopenModal} />}
       <div
         className="relative  h-[100vh] bg-contain bg-center bg-no-repeat  flex items-center justify-center text-black"
         style={{ backgroundImage: "url('/donatebg.png')" }}
       >
         <div className="relative -top-10 p-8 rounded-lg text-center">
-          <h1 className="text-4xl font-bold font-hero-heading tracking-wide">
+          <h1 className="text-4xl lg:text-6xl font-semibold font-hero-heading tracking-wide">
             Together, let's empower the next generation
           </h1>
           <p className="pt-14 font-subtext-heebo">
@@ -25,7 +32,10 @@ const DonateSection = () => {
             </strong>{" "}
             has the tools to succeed.
           </p>
-          <button className="relative top-24 rotating-button bg-black text-white px-6 py-6 w-32 h-32 rounded-full font-bold hover:animate-none hover:text-xl duration-300 hover:bg-navorange hover:text-black border-2 border-black">
+          <button
+            onClick={() => setopenModal(true)}
+            className="relative top-24 rotating-button bg-[#FFF7D1] text-black font-subtext-heebo px-6 py-6 w-32 h-32 rounded-full font-bold hover:animate-none hover:text-xl duration-300 hover:bg-black hover:text-[#FFF7D1] "
+          >
             Donate Now.
           </button>
         </div>
